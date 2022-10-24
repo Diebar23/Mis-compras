79428a55488273be2772ca31e9d7fda642cbc9fc
Diego Barbas <barbasdiego@yahoo.com>
2022-10-24 16:06:48 -0300

    js1

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/* let ciudad = 'Buenos Aires'
+console.log()
+/* alert (ciudad) */
+/* 
+let nombreUsuario = prompt('ingresa tu nombre')
+let edadUsuario = parseInt (prompt('ingresa tu edad')) */
+
+let edadActual = parseInt(prompt('ingresa tu edad hoy'));
+let edadEn5 = edadActual+5;
+alert('en 5 años tendras '+edadEn5+' años.');
+46
+
+let dato = prompt('ingresa tu nombre');
+alert('Bienvenido'+dato);
