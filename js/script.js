c326e6f19164eff6296f676dd0cad4f403cfd516
Diego Barbas <barbasdiego@yahoo.com>
2022-11-06 12:46:21 -0300

    correccion 1er entrega2

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
 
 
 let nombreUsuario = 'diego';
-
+//Ingreso usuario
 function loginUsuario(){
     let ingresar = false;
-        //let  userName = prompt('Ingresá tu Usuario')
-        let  userName = prompt('Ingresá tu Usuario');    
+        let  userName = prompt(`Ingresá tu Usuario`);    
             if (userName == nombreUsuario) {
                 ingresar = true;
                 return ingresar;
             }else{
                 //alert('Usuario no Válido')
-                alert('Usuario no Válido');
-                return ingresar;
+                alert(`Usuario no Válido`);
+                return userName;
             }
 }
 
@@ -20,20 +19,18 @@ loginUsuario();
 
 
 let clavePASS = '1234';
-
+//Ingreso contraseña
 function loginContraseña(){
     let ingresar = false;
-
     for (let i = 2; i >= 0; i--) {
-        //let  userPASS = prompt('Ingresá tu Contraseña. Tenes ' + ( i + 1) + ' oportunidades.')
-        let  userPASS = prompt('Ingresá tu Contraseña. Tenes ' + ( i + 1) + ' oportunidades.');    
+        let  userPASS = prompt(`Ingresá tu Contraseña. Tenes  ${( i + 1)}  oportunidades.`);    
         if (userPASS == clavePASS) {
-            alert('ingreso exitoso. Bienvenido/a');
+            alert(`ingreso exitoso. Bienvenido/a`);
             ingresar = true;
             break;
         }else{
             //alert('Error. Te quedan ' + i + ' Intentos')
-            alert('Error. Te quedan ' + i + ' Intentos');
+            alert(`Error. Te quedan  ${i}  Intentos`);
         }
     }
 
